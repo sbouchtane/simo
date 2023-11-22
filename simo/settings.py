f69a05ca8ca07b68ffbb13f94d8045394d1a03fb
sbouchtane <149199734+sbouchtane@users.noreply.github.com>
2023-11-22 07:46:42 +0100

    Update settings.py

--- simo/settings.py
+++ simo/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d@-pn!q_u@^sq8&ttmx#c_r7l_)nuq94q_=$ok8y=pa(m9zfes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://simoxontheflux.onrender.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
