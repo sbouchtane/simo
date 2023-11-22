0ef7923712ab12e317557d0902355d3bfca399f3
sbouchtane <149199734+sbouchtane@users.noreply.github.com>
2023-11-22 07:53:52 +0100

    Update settings.py

--- simo/settings.py
+++ simo/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d@-pn!q_u@^sq8&ttmx#c_r7l_)nuq94q_=$ok8y=pa(m9zfes'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
